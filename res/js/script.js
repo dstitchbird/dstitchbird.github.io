9909edff37a56d66a97cff753ab1110fd23173b2
Akhil Ashokan <akhilashokan1996@gmail.com>
2020-12-30 21:59:56 +0530

    markup css for desktop done next media query

--- res/js/script.js
+++ res/js/script.js
@@ -9,12 +9,36 @@ function grid_fun(values) {
 }
 
 /* default click */
-document.getElementById('default').click();
+document.getElementById("default").click();
 
+function populateTestCard() {
+  var slides = document.getElementsByClassName("slide");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].innerHTML = `<div class="slideInner">
+    <img
+      src="res/img/grid.svg"
+      height="auto"
+      width="100px"
+      alt=""
+    />
+    <p>
+      Ash's tactics & books have helped me a lot in my
+      understanding on how social media advertising works.I can
+      say that he is one of the best development professionals i
+      have dealt with so far. His experience is great & he is such
+      a great & pleasant person to work with as
+    </p>
+    <h4>Akshit Singh Dutt</h4>
+    <p>Digital Marketing Manager, Peerio Technologies</p>
+  </div>`;
+  }
+}
+populateTestCard();
 /* award tab */
 function handleClick(evt, item) {
-  var i, tabcontent, tablinks ,image;
-  
+  var i, tabcontent, tablinks, image;
+
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
@@ -23,16 +47,14 @@ function handleClick(evt, item) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  tabImage=document.getElementById('awardPlaceholder');
-  if (item==='2011'){
-    tabImage.src="/res/img/workgridbg1.jpg";
-  }else if(item==='2012'){
-    tabImage.src="/res/img/grid.svg";
-  }else if(item==="2015"){
-    tabImage.src="/res/img/workgridbg1.jpg";
+  tabImage = document.getElementById("awardPlaceholder");
+  if (item === "2011") {
+    tabImage.src = "/res/img/workgridbg1.jpg";
+  } else if (item === "2012") {
+    tabImage.src = "/res/img/grid.svg";
+  } else if (item === "2015") {
+    tabImage.src = "/res/img/workgridbg1.jpg";
   }
-  
-  
 
   document.getElementById(item).style.display = "block";
   evt.currentTarget.className += " active";
